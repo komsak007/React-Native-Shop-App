f24ee06fc2d8e012deae90a12a9101fde2613323
komsak007 <lert_zodiac@hotmail.com>
2021-08-10 22:13:58 +0700

    Add Product detail screen and cart screen | function add cart to redux

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
+import CartReducer from "./store/reducers/cart";
 import productsReducer from "./store/reducers/products";
 import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
   products: productsReducer,
+  cart: CartReducer,
 });
 
 const store = createStore(rootReducer);
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -3,16 +3,26 @@ import { createStackNavigator } from "react-navigation-stack";
 import { Platform } from "react-native";
 
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+import CartScreen from "../screens/shop/CartScreen";
 import Colors from "../constants/Colors";
 
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen,
+    Cart: CartScreen,
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+      },
+      headerTitleStyle: {
+        fontFamily: "open-sans-bold",
+      },
+      headerBackTitleStyle: {
+        fontFamily: "open-sans",
       },
       headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
     },
