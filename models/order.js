aef33fd0158f3bee01d6ddc6b756aeaa6a99c04e
komsak007 <lert_zodiac@hotmail.com>
2021-08-14 21:22:35 +0700

    Add style in orderScreen

--- models/order.js
+++ models/order.js
@@ -1,9 +1,23 @@
+import moment from "moment";
+
 class Order {
   constructor(id, items, totalAmount, date) {
     this.id = id;
     this.items = items;
     this.totalAmount = totalAmount;
     this.date = date;
+  }
+
+  get readableDate() {
+    // return this.date.toLocaleDateString("en-EN", {
+    //   year: "numeric",
+    //   mount: "long",
+    //   day: "numeric",
+    //   hour: "2-digit",
+    //   minute: "2-digit",
+    // });
+
+    return moment(this.date).format("D MMMM YYYY, hh:mm");
   }
 }
 
--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
 
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.orders.orders);
@@ -13,7 +14,12 @@ const OrdersScreen = (props) => {
     <FlatList
       data={orders}
       key={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+        />
+      )}
     />
   );
 };
